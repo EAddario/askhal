0f833c479161ba313f3e63f643b07263b9621bea
Ed Addario <eaddario@hotmail.com>
2024-12-03 08:20:55 +0000

    Remove configuration

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    TEMPERATURE: 1.0,       // Range: [0, 2]
-    TOP_P: 1.0,             // Range: (0, 1]
-    TOP_K: 0,               // Range: [0, Infinity) Not available for OpenAI models
-    FREQUENCY_PENALTY: 0,   // Range: [-2, 2]
-    PRESENCE_PENALTY: 0,    // Range: [-2, 2]
-    REPETITION_PENALTY: 1.0 // Range: (0, 2]
-}
